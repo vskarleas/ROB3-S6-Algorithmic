a3d28b8087bcac1bec6eb3fc5e60be03a2ff5a9c
Vasilis Skarleas <51437323+vskarleas@users.noreply.github.com>
2024-02-11 10:23:58 +0100

    Update minmax.c

--- TP/TP2/minmax.c
+++ TP/TP2/minmax.c
@@ -3,12 +3,31 @@
 
 //EXO1.1
 void get_min_max_1(int* tab, int n, int* min, int* max){
-//TODO
+int max_local=tab[0];
+int min_local = tab[0];
+
+for (int i = 1; i<n; i++)
+{
+    if (tab[i] < min_local)
+    { min_local = tab[i];}
+    else if (tab[i] > max_local)
+    { max_local = tab[i];
+        }
+}
+*min = min_local;
+*max = max_local;
+
 }
 
 //EXO1.2
 void get_min_max_rec(int* tab, int from, int to, int* min, int * max){
-//TODO  
+if(to - from == 2)
+{if(tab[from]<tab[to]){*min = tab[from]; *max = tab[to];}}
+else if(to-from != 2)
+{get_min_max_rec(tab, 0, n/2, min, max);
+    get_min_max_rec(tab, (n/2)+1, n-1, min, max);
+}
+
 }
 
 void get_min_max_2(int* tab, int n, int* min, int* max){
